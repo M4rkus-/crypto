f64cd74397492909404ed8621ea321f63f16080c
M4rkus- <M4rkus-@users.noreply.github.com>
2023-01-17 11:35:57 +0100

    Create OTP.py

---
+++ exec/python/OTP.py
@@ -0,0 +1,19 @@
+def encrypt_otp(plaintext, key):
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        ciphertext += chr(ord(plaintext[i]) ^ ord(key[i % len(key)]))
+    return ciphertext
+
+def decrypt_otp(ciphertext, key):
+    plaintext = ""
+    for i in range(len(ciphertext)):
+        plaintext += chr(ord(ciphertext[i]) ^ ord(key[i % len(key)]))
+    return plaintext
+
+# Beispielnutzung
+plaintext = "DIESISTEINBEISPIEL"
+key = "SECRETKEY"
+ciphertext = encrypt_otp(plaintext, key)
+print("Verschlüsselter Text:", ciphertext)
+decrypted_text = decrypt_otp(ciphertext, key)
+print("Entschlüsselter Text:", decrypted_text)
