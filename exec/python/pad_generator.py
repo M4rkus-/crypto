e02a6525639925b6550a40e833f84ac66692d317
M4rkus- <M4rkus-@users.noreply.github.com>
2023-01-17 11:53:56 +0100

    Create pad_generator.py

---
+++ exec/python/pad_generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_otp(length, count, file_name):
+    # Erstellt eine Liste von zufälligen One-Time-Pads
+    pads = []
+    for i in range(count):
+        pad = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        pads.append(pad)
+    # Schreibt die Pads in eine Datei
+    with open(file_name, 'w') as f:
+        for pad in pads:
+            f.write(pad + '\n')
+    return pads
+
+# Beispielaufruf
+pads = generate_otp(16, 5, 'otp.txt')
+print(pads)
